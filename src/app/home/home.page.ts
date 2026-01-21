3f61db4aae6a0e2d8a521c59852b8a5c972c4f85
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2026-01-21 11:36:02 +0100

    [fix] fixed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,34 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import {
-  IonContent,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-} from '@ionic/angular/standalone';
-
-import { ButtonComponent } from '../button/button.component';
-=======
 import { ButtonComponent } from "../button/button.component";
->>>>>>> d385935b4b7821f90bebe87830b3a422b24aa86e
+import { IonicModule } from '@ionic/angular'
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-<<<<<<< HEAD
-  imports: [
-    CommonModule,
-    IonContent,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    ButtonComponent,
-  ],
-=======
   imports: [IonicModule, CommonModule, ButtonComponent],
->>>>>>> d385935b4b7821f90bebe87830b3a422b24aa86e
 })
 export class HomePage { }
