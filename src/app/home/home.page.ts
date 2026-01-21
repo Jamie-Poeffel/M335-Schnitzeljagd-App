f8e2879c0fa966cb592f7e396df41c8a131a2f77
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2026-01-21 11:22:12 +0100

    [feat] merge fix

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, ButtonComponent],
 })
-export class HomePage {}
+export class HomePage { }
