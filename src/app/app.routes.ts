3344533126d7e7d1e68e301e937d1408149d129d
Kush-Srigiri <kushsrigiri@gmail.com>
2026-01-21 11:17:53 +0100

    created a leaderboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'leaderboard',
+    loadComponent: () =>
+      import('./leaderboard/leaderboard.page').then((m) => m.LeaderboardPage),
   },
 ];
--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,21 +1,22 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { IonButton } from "@ionic/angular/standalone";
+import { IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
+  standalone: true,
   styleUrls: ['./button.component.scss'],
   imports: [IonButton],
 })
 export class ButtonComponent implements OnInit {
   @Input() state: 'normal' | 'outline' = 'normal';
   @Input() label: string = 'Button';
-  @Input() children: HTMLElement | string = "";
+  @Input() children: HTMLElement | string = '';
 
   @Output() clicked = new EventEmitter<void>();
 
-  constructor() { }
-  ngOnInit() { }
+  constructor() {}
+  ngOnInit() {}
 
   onClick() {
     this.clicked.emit();
