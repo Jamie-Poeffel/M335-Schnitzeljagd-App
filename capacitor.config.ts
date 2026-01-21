73200e4578291a1ddd6934aeafb6a682c72c3efa
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2026-01-21 10:54:43 +0100

    init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'schnitzeljagt',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    url: 'http://10.10.16.112:4200',
+    cleartext: true
+  }
 };
 
 export default config;
---
+++ src/app/button/button.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IonButton } from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+  imports: [IonButton],
+})
+export class ButtonComponent implements OnInit {
+  @Input() state: 'normal' | 'outline' = 'normal';
+  @Input() label: string = 'Button';
+  @Input() children: HTMLElement | string = "";
+
+  @Output() clicked = new EventEmitter<void>();
+
+  constructor() { }
+  ngOnInit() { }
+
+  onClick() {
+    this.clicked.emit();
+  }
+}
